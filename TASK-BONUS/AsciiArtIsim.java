b55ea298f21d82de3ab42a90d327b48ccb9d3406
BirgulGokturk <250541094@firat.edu.tr>
2025-10-22 12:49:15 +0300

    Add files via upload

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,11 @@
+public class AsciiArtIsim {
+    public static void main(String[] args) {
+        System.out.println(" ____   _                 _ _ ");
+        System.out.println("| __ ) (_) ___ _ __   __| | |");
+        System.out.println("|  _ \\ | |/ _ \\ '_ \\ / _` | |");
+        System.out.println("| |_) || |  __/ | | | (_| |_|");
+        System.out.println("|____(_)_|\\___|_| |_|\\__,_(_)");
+
+        System.out.println("\nASCII Art ile yazılmış: Birgül");
+    }
+}
