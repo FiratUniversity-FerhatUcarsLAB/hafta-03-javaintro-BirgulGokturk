b03b2ec46162f81718dde176f12a2823f5003db1
BirgulGokturk <250541094@firat.edu.tr>
2025-10-22 12:15:44 +0300

    Add files via upload

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,17 @@
+public class IsimKarti {
+ public static void main ( String [] args ) {
+         String ad = " Birgül ";
+         String soyad = "Göktürk ";
+         String ogrNo = "250541094 ";
+         String bolum = "Yazilim Muh .";
+
+        System . out . println (" =================== ") ;
+        System . out . println (" OGRENCI BILGILERI ") ;
+        System . out . println (" =================== ") ;
+        System . out . println ("Ad : " + ad ) ;
+        System . out . println (" Soyad : " + soyad ) ;
+        System . out . println ("No : " + ogrNo ) ;
+        System . out . println (" Bolum : " + bolum ) ;
+        System . out . println (" =================== ") ;
+        }
+}
